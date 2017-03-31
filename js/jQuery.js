f926afe24b5959e9380b299a80249303f860c3ae
tphvtr <otets.odnako@gmail.com>
2017-03-31 12:02:03 +0300

    mobile, desktop menus

--- js/jQuery.js
+++ js/jQuery.js
@@ -26,6 +26,33 @@ $(document).ready(function() {
      });
 });
 
+$(document).ready(function() {
+    var overlay = $('#overlay'); 
+    var open_modal = $('.open_modal_1'); 
+    var close = $('.modal_close, #overlay'); 
+    var modal = $('.modal_div'); 
+     open_modal.click( function(event){
+         event.preventDefault(); 
+         var div = $(this).attr('href');
+         console.log(div);
+         overlay.fadeIn(400, 
+             function(){ 
+                 $(div)
+                     .css('display', 'block') 
+                     .animate({opacity: 1, top: '50%'}, 200); 
+         });
+     });
+
+     close.click( function(){ 
+            modal
+             .animate({opacity: 0, top: '45%'}, 200, 
+                 function(){ 
+                     $(this).css('display', 'none');
+                     overlay.fadeOut(400);
+                 }
+             );
+     });
+});
 
 $("#tv_60").click(function(){
     $("#choosenTarrif").attr("value", "Телебачення 60грн/міс");
